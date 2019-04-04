4ead12ab2d8a91a21152f1a598b4136464c94eba
Martin Smith <msmith3dart@icloud.com>
2019-04-04 23:56:27 +0000

    style fix

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,12 +1,12 @@
 user { 'www-data':
-	ensure => 'present',
-	gid => 'www-data'
+  ensure => 'present',
+  gid    => 'www-data'
 }
 
 file { '/tmp/holberton':
-		ensure => 'present',
-		content => "I love Puppet",
-		mode => '0744',
-		owner => 'www-data',
-		group => 'www-data',
+    ensure  => 'present',
+    content => 'I love Puppet',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
 }
