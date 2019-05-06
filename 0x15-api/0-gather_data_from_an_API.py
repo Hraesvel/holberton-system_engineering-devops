f6a6331aa11bcc4bd4e019c9f295a986be6a1399
martin smith <msmith3d@icloud.com>
2019-05-06 16:35:13 -0700

    check if a user exist

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,16 +10,17 @@ if __name__ == "__main__":
         usr = requests.get(
             "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
         ).json()
-        tasks = requests.get(
-            "{ep}/todos".format(ep=entrypoint),
-            params={"userId": usrId}
-        ).json()
-        nom_t = len(tasks)
-        done_t = [t for t in tasks if t['completed'] is True]
-        print(
-            "Employee {} is done with tasks({}/{})"
-            .format(usr['name'], len(done_t), nom_t)
-        )
-        [print('\t' + t['title']) for t in done_t]
+        if usr:
+            tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+            ).json()
+            nom_t = len(tasks)
+            done_t = [t for t in tasks if t['completed'] is True]
+            print(
+                "Employee {} is done with tasks({}/{})"
+                .format(usr['name'], len(done_t), nom_t)
+            )
+            [print('\t' + t['title']) for t in done_t]
     except Exception as e:
         print(e)
