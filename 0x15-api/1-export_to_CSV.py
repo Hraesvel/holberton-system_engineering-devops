c373f425b03ff54823e0a26cea1d6f105fe798e1
martin smith <msmith3d@icloud.com>
2019-05-06 16:44:51 -0700

    update code, check if a user exists

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -13,28 +13,29 @@ if __name__ == "__main__":
         usr = requests.get(
             "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
         ).json()
-        tasks = requests.get(
-            "{ep}/todos".format(ep=entrypoint),
-            params={"userId": usrId}
-        ).json()
-        nom_t = len(tasks)
-        done_t = [t for t in tasks if t['completed'] is True]
+        if usr:
+            tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+            ).json()
+            nom_t = len(tasks)
+            done_t = [t for t in tasks if t['completed'] is True]
 
-        with open("{usrId}.csv".format(usrId=usrId), "w+") as f:
-            fields = ("id", "username", "status", "title")
-            writer = csv.DictWriter(
-                    f,
-                    fieldnames=fields,
-                    lineterminator='\n',
-                    quotechar='"',
-                    quoting=csv.QUOTE_ALL
-                    )
-            for task in tasks:
-                writer.writerow(dict(
-                    id=task['userId'],
-                    username=usr["name"],
-                    status=task["completed"],
-                    title=task["title"]
-                ))
+            with open("{usrId}.csv".format(usrId=usrId), "w+") as f:
+                fields = ("id", "username", "status", "title")
+                writer = csv.DictWriter(
+                        f,
+                        fieldnames=fields,
+                        lineterminator='\n',
+                        quotechar='"',
+                        quoting=csv.QUOTE_ALL
+                        )
+                for task in tasks:
+                    writer.writerow(dict(
+                        id=task['userId'],
+                        username=usr["name"],
+                        status=task["completed"],
+                        title=task["title"]
+                    ))
     except Exception as e:
         print(e)
