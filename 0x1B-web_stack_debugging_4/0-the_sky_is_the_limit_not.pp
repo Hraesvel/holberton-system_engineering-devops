071b25c4d7bcc9d1676b91f05b39ea44366f9a37
Martin Smith <msmith3dart@icloud.com>
2019-05-30 21:29:16 -0700

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
-# I'll fix your code!
+# this file will increase the ULIMIT so taht workers can handel more open files.
 file { '/etc/default/nginx':
   ensure => present,
-} -> exec { 'increase fs.open':
+} -> exec { 'increase ULIMIT':
   path    => '/bin/',
   command => "sed -i 's/15/1024/g' /etc/default/nginx"
 }
