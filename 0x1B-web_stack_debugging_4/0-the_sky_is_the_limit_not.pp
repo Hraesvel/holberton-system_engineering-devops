16b367d8bf3c69096ce17b2656950fb33f6e6ed7
Martin Smith <msmith3dart@icloud.com>
2019-05-30 21:37:11 -0700

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,13 @@
 # this file will increase the ULIMIT so taht workers can handel more open files.
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
+}
+
 file { '/etc/default/nginx':
   ensure => present,
 } -> exec { 'increase ULIMIT':
+  notify => Service['nginx'], # set up a relationship
   path    => '/bin/',
   command => "sed -i 's/15/1024/g' /etc/default/nginx"
 }
